0659bc63256f2960229cc673b608f2436040cbc1
Sunnyg23 <sunbirghag@hotmail.com>
2017-04-23 23:28:23 +0100

    first commit

--- config/routes.js
+++ config/routes.js
@@ -2,5 +2,17 @@ const express = require('express');
 const router  = express.Router();
 
 router.get('/', (req, res) => res.render('statics/home'));
+router.get('/statics/login', (req, res) => res.render('statics/login'));
+router.get('/', (req, res) => res.render('statics/register'));
+router.get('/', (req, res) => res.render('statics/profile'));
+router.get('/', (req, res) => res.render('statics/add'));
+router.get('/', (req, res) => res.render('statics/edit'));
+router.get('/', (req, res) => res.render('statics/update'));
+router.get('/', (req, res) => res.render('statics/delete'));
+router.get('/', (req, res) => res.render('statics/followers'));
+router.get('/', (req, res) => res.render('statics/following'));
+router.get('/', (req, res) => res.render('statics/account'));
+
+
 
 module.exports = router;
